6cb849518a6b986d5ffa3063160ddc24379bb684
Chris Guida <chrisguida@gmail.com>
2022-05-10 14:54:07 -0500

    update to 0.9.7, update properties

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -11,13 +11,20 @@ cat << EOF > /data/start9/stats.yaml
 ---
 version: 2
 data:
+  Quick Connect URL:
+    type: string
+    value: $TOR_ADDRESS:50001:t
+    description: For scanning into wallets such as BlueWallet
+    copyable: true
+    qr: true
+    masked: true
   Hostname:
     type: string
     value: $TOR_ADDRESS
     description: Hostname to input into wallet software such as Sparrow.
     copyable: true
     qr: false
-    masked: false
+    masked: true
   Port:
     type: string
     value: "50001"
