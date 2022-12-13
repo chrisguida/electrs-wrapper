6f92ab4dd4f02a79f99473da60c73d7073c370d2
gStart9 <george@start9labs.com>
2022-12-13 16:27:16 -0700

    Avoid test 'unaryoperator expected error' when curl fails to get blockheight

--- check-synced.sh
+++ check-synced.sh
@@ -32,12 +32,12 @@ else
     features_res=$(echo '{"jsonrpc": "2.0", "method": "server.features", "params": ["", "1.4"], "id": 0}' | netcat -q 1 127.0.0.1 50001)
     synced_height=$(curl -sS localhost:4224 | grep index_height | grep tip | awk '{ print $NF }')
     error_code=$?
-    if [ $error_code -ne 0 ]; then
+    if [[ $error_code -ne 0 ]]; then
         echo $synced_height >&2
         exit $error_code
     fi
 
-    if [ $synced_height -lt $b_block_count ]; then
+    if [[ $synced_height -lt $b_block_count ]]; then
         echo "Catching up to blocks from bitcoind. This should take at most a day. Progress: $synced_height of $b_block_count blocks" >&2
         exit 61
     elif [ -z $features_res ]; then
